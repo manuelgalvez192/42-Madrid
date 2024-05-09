f339c832e8777d0025285878a5340002b30784dc
Manuel Galvez <60622528+manuelgalvez192@users.noreply.github.com>
2024-05-09 21:16:35 +0200

    queda arreglar el error de doble free

--- getNextLine/get_next_line.c
+++ getNextLine/get_next_line.c
@@ -16,7 +16,7 @@ int find_new_line(char *str)
 {
 	int count = 0; 
 
-	while(str[count])
+	while (str[count])
 	{
 		if (str[count] == '\n')
 			return(1);
@@ -25,40 +25,59 @@ int find_new_line(char *str)
 	return(0);
 }
 
-char *cut_line(char *line, char *buffer)
+char *cut_line(char *line, char *buffer, char *new_line)
 {
-	int i = 0;
+	int i;
+	int j;
 	int len_line = ft_strlen(line);
-	while(buffer[i] != '\n')
+
+	i = 0;
+	while (buffer[i] != '\n')
 	{
 		line[len_line] = buffer[i];
 		len_line++;
 		i++;
 	}
-	line[len_line] = '\n';
+	j = 0;
+	while (buffer[i])
+	{
+		new_line[j] = buffer[i];
+		i++;
+		j++;
+	}
+	while (new_line[j])
+	{
+		new_line[j] = '\0';
+		j++;
+	}
 	return(line);
 }
 
 char	*get_next_line(int fd)
 {
+	static char	*new_line;
 	char		*buffer;
 	char		*line;
-	//char		*new_line;
 	size_t		bytes_read;
 
 	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	line = ft_calloc(1, sizeof(char));
-	if (fd < 0 || !buffer || !line || BUFFER_SIZE <= 0)
+	if (!new_line)
+		new_line = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	line = ft_strjoin(line, new_line);
+	if (fd < 0 || !buffer || !line || BUFFER_SIZE <= 0 || !new_line)
 		return (NULL);
 	bytes_read = read(fd, buffer, BUFFER_SIZE);
 	while (bytes_read > 0)
 	{
 		if(find_new_line(buffer))
 		{
-			return(cut_line(line, buffer));
+			return(cut_line(line, buffer, new_line));
 		}
 		line = ft_strjoin(line, buffer);
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 	}
+	free (new_line);
+	free (buffer);
 	return (line);
 }
